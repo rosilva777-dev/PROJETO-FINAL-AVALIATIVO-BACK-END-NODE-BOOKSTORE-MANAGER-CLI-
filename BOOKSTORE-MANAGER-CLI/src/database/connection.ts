745e9bef937bd9e7fac4cbe64c7798d5f94a236f
Romário Silva <rosilvasilva777@gmail.com>
2026-07-25 20:11:10 -0300

    add: reparos e informações alteradas neste arquivo.

--- BOOKSTORE-MANAGER-CLI/src/database/connection.ts
+++ BOOKSTORE-MANAGER-CLI/src/database/connection.ts
@@ -1,15 +1,15 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
+import config from "../config/index.ts";
 
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+
+    host: config.host,
+    port: config.port,
+    database: config.database,
+    user: config.user,
+    password: config.password,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000
+
 });
